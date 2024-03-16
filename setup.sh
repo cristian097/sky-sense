0c7a8c6956ba0df6e25b1a01780beabfb7606cea
cristian097 <118420369+cristian097@users.noreply.github.com>
2024-03-15 22:55:47 -0500

    Delete setup.sh

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Eliminar archivos temporales de pip
-rm -r ~/.cache/pip
-rm -r build
-
-# Actualizar el entorno virtual (si es necesario)
-/home/adminuser/venv/bin/python -m pip install --upgrade virtualenv
